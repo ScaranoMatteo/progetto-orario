405385c4316d76db74616b3fe1317d31441dc279
Scarano-Davide <76488497+Scarano-Davide@users.noreply.github.com>
2020-12-28 16:07:43 +0100

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-//
+// vettore che contiene l'orario settimanale
 var orario=[
       ["Domenica"],
       ["Lunedì", "Telecomunicazioni", "Telecomunicazioni", "Telecomunicazioni", "Storia", "Robotica", "Robotica", null, null],
@@ -9,7 +9,7 @@ var orario=[
       ["Sabato", "T.P.S.I.T.", "T.P.S.I.T.", "Informatica", "Informatica", "Informatica", null, null, null]
   ];
 
-//
+// vettore che contiene i giorni di vacanza
 var giorniDiVacanza=[
       // Giorno dei morti
       [2, 10, 2020, "Giorno dei morti", "fas fa-cross"],
@@ -56,100 +56,139 @@ var giorniDiVacanza=[
       [2, 5, 2021, "Festa della Repubblica", "fas fa-university"],
   ];
 
+// funzione che viene chiamata quando il file index.html è stata caricato
 function paginaCaricataOrarioOdierno() {
-  //
-  var oggi=new Date(2020, 11, 26, 8, 15);
-
-  //
+  var oggi=new Date(2020, 11, 21, 12, 58);
   var totaleMinuti=oggi.getHours()*60+oggi.getMinutes();
-
-  //
   orarioOdierno(oggi, oggi.getDay(), totaleMinuti);
 }
 
 function orarioOdierno(oggi, i, totaleMinuti) {
-  if(giornoSenzaScuola(oggi)) {
-    //
-    document.getElementById("tabella").remove();
+  // controllo se l'anno scolastico non è ancora iniziato o se è finito
+  if(annoScolasticoNonIniziato(oggi) || annoScolasticoFinito(oggi)) {
+    aggiungiInfo("far fa-calendar", annoScolasticoNonIniziato(oggi)?"L'anno scolastico non è  ancora iniziato!":"L'anno scolastico è finito!");
+  }
+  // controllo se oggi è un giorno di vacanza
+  else if(giornoDiVacanza(oggi)) {
+    aggiungiInfo(iconaGiornoDiVacanza(oggi), testoGiornoDiVacanza(oggi));
+  }
+  // controllo se oggi è domenica
+  else if(i==0) {
+    aggiungiInfo("far fa-calendar-times", "Oggi è Domenica!");
+  }
+  // controllo se non è domenica e se le lezioni non sono ancora iniziate
+  else if(i!=0 && totaleMinuti<480) {
+    riempiTabellaOrarioOdierno(i);
 
-    //
-    var image=document.createElement("img");
-    image.setAttribute("src", "img/school-closed.jpg");
-    image.setAttribute("alt", "scuola chiusa");
-    image.setAttribute("class", "img-fluid");
-    document.getElementById("info").appendChild(image);
-  } else if(giornoDiVacanza(oggi)) {
-    //
-    document.getElementById("tabella").remove();
+    aggiungiMessaggioDiAvviso("danger", "Le lezioni di oggi devono ancora iniziare!", false);
+  }
+  // controllo se le lezioni sono finite nei vari casi (giorni normali, sabato e mercoledi)
+  else if((i!=0 && i!=3 && i!=6 && totaleMinuti>=780) || (i==6 && totaleMinuti>=730) || (i==3 && totaleMinuti>=1040)) {
+    riempiTabellaOrarioOdierno(i);
 
-    //
-    var image=document.createElement("img");
-    image.setAttribute("src", "img/school-closed.jpg");
-    image.setAttribute("alt", "scuola chiusa");
-    image.setAttribute("class", "img-fluid");
-    document.getElementById("info").appendChild(image);
+    aggiungiMessaggioDiAvviso("success", "Le lezioni di oggi sono finite!", false);
+  }
+  // controllo se è mercoledì e se le lezioni non sono ancora finite
+  else if(i==3 && totaleMinuti>=680 && totaleMinuti<940) {
+    riempiTabellaOrarioOdierno(i);
 
-    //
-    var title=document.createElement("h3");
-    //
+    aggiungiMessaggioDiAvviso("warning", null, true)
+  }
+  // controllo se non è domenica
+  else if(i!=0) {
+    riempiTabellaOrarioOdierno(i);
+
+    evidenziaMateriaAttuale(i, totaleMinuti);
+
+    // creo un contenitore (div)
+    var div=document.createElement("div");
+    div.className="info-orario-odierno";
+
+    // creo tre paragrafi (p)
+    var paragraph1=aggiungiParagrafo("Materia attuale: ", materiaAttuale(i, totaleMinuti));
+    var paragraph2=aggiungiParagrafo("Tempo rimanente alla fine della lezione: ", minutiRimanenti(totaleMinuti)==1?minutiRimanenti(totaleMinuti)+" minuto":minutiRimanenti(totaleMinuti)+" minuti");
+    var paragraph3=aggiungiParagrafo("Materia successiva: ", materiaSuccessiva(i, totaleMinuti));
+
+    // creo un link (a)
+    var a=document.createElement("a");
+    a.setAttribute("href", "orario-completo.html");
     var icon=document.createElement("i");
-    icon.setAttribute("class", iconaGiornoDiVacanza(oggi)+" icona-vacanza");
-    title.appendChild(icon);
-    //
+    icon.setAttribute("class", "far fa-calendar-alt icona-link");
+    a.appendChild(icon);
     var span=document.createElement("span");
-    var text=document.createTextNode(testoGiornoDiVacanza(oggi));
+    var text=document.createTextNode("Visualizza Orario completo");
     span.appendChild(text);
-    title.appendChild(span);
-    document.getElementById("info").appendChild(title);
-  } else if(i==0) {
-    //
-    document.getElementById("tabella").remove();
+    a.appendChild(span);
 
-    //
-    var image=document.createElement("img");
-    image.setAttribute("src", "img/school-closed.jpg");
-    image.setAttribute("alt", "scuola chiusa");
-    image.setAttribute("class", "img-fluid");
-    document.getElementById("info").appendChild(image);
+    // aggiungo i paragrafi e il link al contenitore
+    div.appendChild(paragraph1);
+    div.appendChild(paragraph2);
+    div.appendChild(paragraph3);
+    div.appendChild(a);
 
-    //
-    var title=document.createElement("h3");
-    //
-    var icon=document.createElement("i");
-    icon.setAttribute("class", "far fa-calendar-times icona-vacanza");
-    title.appendChild(icon);
-    //
-    var span=document.createElement("span");
-    var text=document.createTextNode("Oggi è Domenica!");
-    span.appendChild(text);
-    title.appendChild(span);
-    document.getElementById("info").appendChild(title);
-  } else if(i!=0 && totaleMinuti<480) {
-    riempiTabella(i);
-
-    var div=document.createElement("div");
-    div.setAttribute("class", "alert alert-danger messaggio");
-    var text=document.createTextNode("Le lezioni di oggi devono ancora iniziare!");
-    div.appendChild(text);
-
-    //
+    // aggiungo il contenitore creato nel contenitore info
     document.getElementById("info").appendChild(div);
-  } else if((i!=0 && i!=3 && i!=6 && totaleMinuti>=780) || (i==6 && totaleMinuti>=730) || (i==3 && totaleMinuti>=1040)) {
-    riempiTabella(i);
+  }
+}
 
-    var div=document.createElement("div");
-    div.setAttribute("class", "alert alert-success messaggio");
-    var text=document.createTextNode("Le lezioni di oggi sono finite!");
-    div.appendChild(text);
+// funzione che controlla se l'anno scolastico non è ancora iniziato
+function annoScolasticoNonIniziato(oggi) {
+  var oggiInMillisecondi=oggi.getTime();
+  var inizioScuola=new Date(2020, 8, 14, 8, 0);
 
-    //
-    document.getElementById("info").appendChild(div);
-  } else if(i==3 && totaleMinuti>=680 && totaleMinuti<940) {
-    riempiTabella(i);
+  if(oggiInMillisecondi<inizioScuola.getTime()) {
+    return true;
+  }
+}
 
-    var div=document.createElement("div");
-    div.setAttribute("class", "alert alert-warning messaggio");
+// funzione che controlla se l'anno scolastico è finito
+function annoScolasticoFinito(oggi) {
+  var oggiInMillisecondi=oggi.getTime();
+  var fineScuola=new Date(2021, 5, 10, 13, 0);
 
+  if(oggiInMillisecondi>fineScuola.getTime()) {
+    return true;
+  }
+}
+
+// funzione che controlla se oggi è un giorno di vacanza
+function giornoDiVacanza(oggi) {
+  for(var i=0; i<giorniDiVacanza.length; i++) {
+    if(giorniDiVacanza[i][0]==oggi.getDate() && giorniDiVacanza[i][1]==oggi.getMonth() && giorniDiVacanza[i][2]==oggi.getFullYear()) {
+      return true;
+    }
+  }
+}
+
+// funzione che rimuove la tabella e aggiunge delle informazioni per i giorni di vacanza, le domeniche e i giorni che non rientrano nell'anno scolastico corrente
+function aggiungiInfo(icona, testo) {
+  document.getElementById("tabella").remove();
+
+  // creo un'immagine
+  var image=document.createElement("img");
+  image.setAttribute("src", "img/school-closed.jpg");
+  image.setAttribute("alt", "scuola chiusa");
+  image.setAttribute("class", "img-fluid");
+  document.getElementById("info").appendChild(image);
+
+  // creo un titolo (icona e testo)
+  var title=document.createElement("h3");
+  var icon=document.createElement("i");
+  icon.setAttribute("class", icona+" icona-vacanza");
+  title.appendChild(icon);
+  var span=document.createElement("span");
+  var text=document.createTextNode(testo);
+  span.appendChild(text);
+  title.appendChild(span);
+  document.getElementById("info").appendChild(title);
+}
+
+// funzione che crea un messaggio di avviso se le lezioni non sono iniziate, sono finite o devono ancora finire
+function aggiungiMessaggioDiAvviso(tipologia, testo, mercoledi) {
+  var div=document.createElement("div");
+  div.setAttribute("class", "alert alert-"+tipologia+" messaggio");
+
+  if(mercoledi) {
     var paragraph=document.createElement("p");
     paragraph.setAttribute("class", "testo-messaggio");
     var text=document.createTextNode("Le lezioni di oggi non sono ancora finite!");
@@ -161,101 +200,32 @@ function orarioOdierno(oggi, i, totaleMinuti) {
     var text=document.createTextNode("Educazione fisica inizia alle 15:40.");
     paragraph.appendChild(text);
     div.appendChild(paragraph);
-
-    //
-    document.getElementById("info").appendChild(div);
-  } else if(i!=0) {
-    riempiTabella(i);
-
-    evidenziaMateriaAttuale(i, totaleMinuti);
-
-    //
-    //
-    var div=document.createElement("div");
-    div.className="info-orario-odierno";
-
-    //
-    var paragraph1=document.createElement("p");
-    //
-    var b=document.createElement("b");
-    var text=document.createTextNode("Materia attuale: ");
-    b.appendChild(text);
-    paragraph1.appendChild(b);
-    //
-    var span=document.createElement("span");
-    var text=document.createTextNode(materiaAttuale(i, totaleMinuti));
-    span.appendChild(text);
-    paragraph1.appendChild(span);
-
-    //
-    var paragraph2=document.createElement("p");
-    //
-    var b=document.createElement("b");
-    var text=document.createTextNode("Tempo rimanente alla fine della lezione: ");
-    b.appendChild(text);
-    paragraph2.appendChild(b);
-    //
-    var span=document.createElement("span");
-    var text=document.createTextNode(minutiRimanenti(totaleMinuti)==1?minutiRimanenti(totaleMinuti)+" minuto":minutiRimanenti(totaleMinuti)+" minuti");
-    span.appendChild(text);
-    paragraph2.appendChild(span);
-
-    //
-    var paragraph3=document.createElement("p");
-    //
-    var b=document.createElement("b");
-    var text=document.createTextNode("Materia successiva: ");
-    b.appendChild(text);
-    paragraph3.appendChild(b);
-    //
-    var span=document.createElement("span");
-    var text=document.createTextNode(materiaSuccessiva(i, totaleMinuti));
-    span.appendChild(text);
-    paragraph3.appendChild(span);
-
-    //
-    var a=document.createElement("a");
-    a.setAttribute("href", "orario-completo.html");
-    //
-    var icon=document.createElement("i");
-    icon.setAttribute("class", "far fa-calendar-alt icona-link");
-    a.appendChild(icon);
-    //
-    var span=document.createElement("span");
-    var text=document.createTextNode("Visualizza Orario completo");
-    span.appendChild(text);
-    a.appendChild(span);
-
-    //
-    div.appendChild(paragraph1);
-    div.appendChild(paragraph2);
-    div.appendChild(paragraph3);
-    div.appendChild(a);
-
-    //
-    document.getElementById("info").appendChild(div);
+  } else {
+    var text=document.createTextNode(testo);
+    div.appendChild(text);
   }
+
+  document.getElementById("info").appendChild(div);
 }
 
-function giornoSenzaScuola(oggi) {
-  var oggiInMillisecondi=oggi.getTime();
-  var inizioScuola=new Date(2020, 8, 14, 8, 0);
-  var fineScuola=new Date(2021, 5, 10, 13, 0);
+// funzione che crea un paragrafo
+function aggiungiParagrafo(titolo, testo) {
+  var paragraph=document.createElement("p");
 
-  //
-  if((oggiInMillisecondi<inizioScuola.getTime()) || (oggiInMillisecondi>fineScuola.getTime())) {
-    return true;
-  }
+  var b=document.createElement("b");
+  var text=document.createTextNode(titolo);
+  b.appendChild(text);
+  paragraph.appendChild(b);
+
+  var span=document.createElement("span");
+  var text=document.createTextNode(testo);
+  span.appendChild(text);
+  paragraph.appendChild(span);
+
+  return paragraph;
 }
 
-function giornoDiVacanza(oggi) {
-  for(var i=0; i<giorniDiVacanza.length; i++) {
-    if(giorniDiVacanza[i][0]==oggi.getDate() && giorniDiVacanza[i][1]==oggi.getMonth() && giorniDiVacanza[i][2]==oggi.getFullYear()) {
-      return true;
-    }
-  }
-}
-
+// funzione che restituisce l'icona per un determinato giorno di vacanza
 function iconaGiornoDiVacanza(oggi) {
   for(var i=0; i<giorniDiVacanza.length; i++) {
     if(giorniDiVacanza[i][0]==oggi.getDate() && giorniDiVacanza[i][1]==oggi.getMonth() && giorniDiVacanza[i][2]==oggi.getFullYear()) {
@@ -264,6 +234,7 @@ function iconaGiornoDiVacanza(oggi) {
   }
 }
 
+// funzione che restituisce il testo per un determinato giorno di vacanza
 function testoGiornoDiVacanza(oggi) {
   for(var i=0; i<giorniDiVacanza.length; i++) {
     if(giorniDiVacanza[i][0]==oggi.getDate() && giorniDiVacanza[i][1]==oggi.getMonth() && giorniDiVacanza[i][2]==oggi.getFullYear()) {
@@ -272,16 +243,16 @@ function testoGiornoDiVacanza(oggi) {
   }
 }
 
-function riempiTabella(i) {
-  //
+// funzione che riempie la tabella dell'orario odierno
+function riempiTabellaOrarioOdierno(i) {
   document.getElementById("giorno").innerHTML=orario[i][0];
 
-  //
   for(var j=1; j<9; j++) {
     document.getElementById("materia-"+j).innerHTML=orario[i][j];
   }
 }
 
+// funzione che evidenzia la materia attuale
 function evidenziaMateriaAttuale(i, totaleMinuti) {
   if(totaleMinuti>=480 && totaleMinuti<530) {
     document.getElementById("riga-1").style.backgroundColor="lightgrey";
@@ -310,6 +281,7 @@ function evidenziaMateriaAttuale(i, totaleMinuti) {
   }
 }
 
+// funzione che restituisce la materia attuale
 function materiaAttuale(i, totaleMinuti) {
   if(totaleMinuti>=480 && totaleMinuti<530) {
     return orario[i][1];
@@ -330,6 +302,7 @@ function materiaAttuale(i, totaleMinuti) {
   }
 }
 
+// funzione che restituisce i minuti rimanenti alla fine della lezione
 function minutiRimanenti(totaleMinuti) {
   if(totaleMinuti>=480 && totaleMinuti<530) {
     return 530-totaleMinuti;
@@ -350,6 +323,7 @@ function minutiRimanenti(totaleMinuti) {
   }
 }
 
+// funzione che restituisce la materia successiva se possibile
 function materiaSuccessiva(i, totaleMinuti) {
   if(totaleMinuti>=480 && totaleMinuti<530) {
     var materiaSuccessiva=orario[i][2];
@@ -369,7 +343,6 @@ function materiaSuccessiva(i, totaleMinuti) {
     var materiaSuccessiva=null;
   }
 
-  //
   if(materiaSuccessiva!=null) {
     return materiaSuccessiva;
   } else {
@@ -377,10 +350,23 @@ function materiaSuccessiva(i, totaleMinuti) {
   }
 }
 
+// funzione che viene chiamata quando il file orario-completo.html è stata caricato
 function paginaCaricataOrarioCompleto() {
-
+  riempiTabellaOrarioCompleto();
 }
 
+// funzione che riempie la tabella dell'orario completo
+function riempiTabellaOrarioCompleto() {
+  var k=1;
+  for(var i=1; i<7; i++) {
+    for(var j=1; j<9; j++) {
+      document.getElementById("lezione-"+k).innerHTML=orario[i][j];
+      k++;
+    }
+  }
+}
+
+// funzione che filtra la tabella dell'orario completo per una materia
 function filtroMaterie(materia) {
   for(var i=1; i<49; i++) {
     var lezione=document.getElementById("lezione-"+i);
@@ -394,6 +380,7 @@ function filtroMaterie(materia) {
   }
 }
 
+// funzione che azzera il filtro
 function azzeraFiltro() {
   for(var i=1; i<49; i++) {
     var lezione=document.getElementById("lezione-"+i);
