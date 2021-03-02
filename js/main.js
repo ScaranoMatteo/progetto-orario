a4768bd358675dcf0592f595a11ef509ff80d75f
ScaranoMatteo <48943683+ScaranoMatteo@users.noreply.github.com>
2021-03-02 17:21:19 +0100

    Update main.js

--- js/main.js
+++ js/main.js
@@ -58,7 +58,7 @@ var giorniDiVacanza=[
 
 // funzione che viene chiamata quando il file index.html è stata caricato
 function paginaCaricataOrarioOdierno() {
-  var oggi=new Date(2021,1,22,8,45);
+  var oggi=new Date();
   var totaleMinuti=oggi.getHours()*60+oggi.getMinutes();
   riempiPagina(oggi, oggi.getDay(), totaleMinuti);
 }
